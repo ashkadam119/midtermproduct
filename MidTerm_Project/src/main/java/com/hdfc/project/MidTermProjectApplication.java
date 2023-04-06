5fe4bb8c44e91980173954d2c856c6cd133b9099
ashkadam119 <ashkadam119@gmail.com>
2023-04-07 00:07:48 +0530

    first commit

---
+++ MidTerm_Project/src/main/java/com/hdfc/project/MidTermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hdfc.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidTermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidTermProjectApplication.class, args);
+	}
+
+}
---
+++ MidTerm_Project/src/test/java/com/hdfc/project/MidTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidTermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
